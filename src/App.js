69aac10f692df7c4ced23387d4255dae689adbcf
Clint Lawson <clint_lwsn@hotmail.com>
2021-02-05 00:40:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+
+import AuthToDoApp from './components/AuthToDoApp/AuthToDoApp'
+import * as msal from "@azure/msal-browser";
+import { MsalProvider } from '@azure/msal-react';
+
+
+const configuration = {
+  auth: {
+      clientId: "1877b32e-fba5-4a06-a0fd-6f867ba4ea01",
+      authority: "https://login.microsoftonline.com/common",
+      redirectUri: "http://localhost:3000",
+  },
+  cache: {
+    cacheLocation: "localStorage", // This configures where your cache will be stored
+    // storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  // system:{
+    // windowHashTimeout: 60000,
+    // iframeHashTimeout: 6000,
+    // loadFrameTimeout: 0,
+    // asyncPopups: true
+  // }
+};
+
+const pca = new msal.PublicClientApplication(configuration);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'wrapper'} >
+      <MsalProvider instance={pca}>
+        <AuthToDoApp />
+      </MsalProvider>
+
     </div>
   );
 }
